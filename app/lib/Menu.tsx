716d173322f19884b15ee409a21c42a4e231f006
orlandoc0107 <orlandocardenas0107@gmail.com>
2023-11-08 14:15:54 -0300

    commit 2

--- app/lib/Menu.tsx
+++ app/lib/Menu.tsx
@@ -1,8 +1,14 @@
-'use client'
+"use client";
 
 import { useState } from "react";
-import { Github, Sobre, Linkedin, Hogar } from "@/app/ui/components/images/icons";
+import {
+  Github,
+  Sobre,
+  Linkedin,
+  Hogar,
+} from "@/app/ui/components/images/icons";
 import Image from "next/image";
+import Link from "next/link";
 
 export function MenuP() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -22,33 +28,58 @@ export function MenuP() {
         className="cursor-pointer"
       />
       {menuOpen && (
-        <div className="menu-overlay fixed inset-y-0 left-0 w-1/4 bg-stone-800 p-4 z-50">
-          <ul className="text-white flex flex-col space-y-4">
-            <li className="flex items-center">
-              <div className="w-6 h-6">
-                <Hogar />
-                <span className="ml-2">Home</span>
+        <div className="menu-overlay fixed inset-y-0 left-0 w-screen bg-sky-900 z-50 justify-center items-center place-items-center">
+          <div className="mx-60 my-10">
+          <div>
+                <button onClick={toggleMenu}>Cerrar</button>
               </div>
-            </li>
-            <li className="flex items-center">
-              <div className="w-6 h-6">
-                <Github />
-                <span className="ml-2">Github</span>
+            <details
+              className="open:bg-white dark:open:bg-slate-900 open:ring-1 open:ring-black/5 dark:open:ring-white/10 open:shadow-lg p-6 rounded-lg"
+              open
+            >
+
+              <summary className="text-sm leading-6 text-slate-900 dark:text-white font-semibold select-none">
+                Menu
+              </summary>
+              <div className="flex flex-col gap-2 overflow-hidden mt-3 text-sm leading-6 text-slate-600 dark:text-slate-400">
+                <div>
+                  <Link href="/">Home</Link>
+                </div>
+                <div>
+                  <Link href="https://github.com/Orlandoc0107" target="_blank">
+                    GitHub
+                  </Link>
+                </div>
+                <div>
+                  <Link
+                    href="https://www.linkedin.com/in/orlando-cardenas-villegas-7a2390248/"
+                    target="_blank"
+                  >
+                    Linkedin
+                  </Link>
+                </div>
+                <div>
+                  <Link href="/proyect">Proyect</Link>
+                </div>
+                <div>
+                  <Link href="/contact">Contact</Link>
+                </div>
               </div>
-            </li>
-            <li className="flex items-center">
-              <div className="w-6 h-6">
-                <Sobre />
-                <span className="ml-2">Contact</span>
+            </details>
+            <details
+              className="open:bg-white dark:open:bg-slate-900 open:ring-1 open:ring-black/5 dark:open:ring-white/10 open:shadow-lg p-6 rounded-lg"
+              open
+            >
+              <summary className="text-sm leading-6 text-slate-900 dark:text-white font-semibold select-none">
+                Topic
+              </summary>
+              <div className="flex flex-col gap-2 overflow-hidden mt-3 text-sm leading-6 text-slate-600 dark:text-slate-400">
+                <div>
+                  <Link href="/">Proximamente</Link>
+                </div>
               </div>
-            </li>
-            <li className="flex items-center">
-              <div className="w-6 h-6">
-                <Linkedin />
-                <span className="ml-2">Linkedin</span>
-              </div>
-            </li>
-          </ul>
+            </details>
+          </div>
         </div>
       )}
     </div>
--- app/lib/inputs.tsx
+++ app/lib/inputs.tsx
@@ -1,13 +1,15 @@
-
+import { Busqueda } from '@/app/ui/components/images/icons'
 
 export function Search() {
   return (
     <div>
-      <input type="text" className="
-      border-slate-200 
-      placeholder-slate-900 
-      contrast-more:border-slate-900 
-      contrast-more:placeholder-slate-900"/>
+<label className="relative block">
+  <span className="sr-only">Search</span>
+  <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+    <Busqueda/>
+  </span>
+  <input className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Search ..." type="text" name="search"/>
+</label>
     </div>
   )
 }
