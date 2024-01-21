03aa36432d88a8d12b685f737b0d9a0724351d6a
orlandoc0107 <orlandocardenas0107@gmail.com>
2024-01-21 19:04:50 -0300

    update

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,22 +1,13 @@
 import Link from 'next/link'
+import styles from '@/app/ui/home.module.css'
 
 export default function NotFound() {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-transpare">
-      <h2 className="text-4xl text-gray-800 font-bold">No Funciona la Ruta</h2>
-      <p className="text-lg text-gray-600 mt-4">Could not find the requested resource.</p>
+    <div className={styles.main}>
+      <h2>No Funciona la Ruta</h2>
+      <p >Could not find the requested resource.</p>
       <Link href="/">
-        <div 
-                className="
-                font-bold px-3 py-2 
-                dark:text-slate-200 
-                text-slate-700 
-                rounded-lg 
-                dark:hover:bg-slate-900 
-                hover:bg-slate-100
-                dark:hover:text-slate-200 
-                hover:text-slate-900
-                ">
+        <div className={styles.Link}>
         Return Home
         </div>
       </Link>
